6828714cbaff0e6d11c6017d4c63188fe11d5680
adrien pelegri <pelegriadrien@gmail.com>
2018-04-18 17:49:06 +0800

    remove all item from cart action

--- src/app/components/cart/CartList.vue
+++ src/app/components/cart/CartList.vue
@@ -3,7 +3,10 @@
     <div class="cart--header has-text-centered">
       <i class="fa fa-2x fa-shopping-cart"></i>
     </div>
-    <ul>
+    <p v-if="cartItems.length === 0" class="cart-empty-text has-text-centered">
+      Add some items to the cart!
+    </p>
+    <ul v-if="cartItems.length > 0">
       <li v-for="cartItem in cartItems" :key="cartItem.id" class="cart-item">
         <CartListItem :cartItem="cartItem" />
       </li>
@@ -11,20 +14,21 @@
         <p>Total Quantity:
           <span class="has-text-weight-bold">
             {{ cartQuantity }}
-          </span></p>
-        <p class="cart-remove-all--text">
+          </span>
+        </p>
+        <p @click="removeAllCartItems" class="cart-remove-all--text">
           <i class="fa fa-trash"></i>Remove all
         </p>
       </div>
     </ul>
-    <button class="button is-primary">
+    <button :disabled="cartItems.length === 0" class="button is-primary">
       Checkout (<span class="has-text-weight-bold">${{ cartTotal }}</span>)
     </button>
   </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex';
+import {mapGetters, mapActions} from 'vuex';
 import CartListItem from './CartListItem';
 
 export default {
@@ -34,6 +38,9 @@ export default {
   },
   created() {
     this.$store.dispatch('getCartItems');
+  },
+  methods: {
+    ...mapActions(['removeAllCartItems'])
   },
   components: {
     CartListItem
